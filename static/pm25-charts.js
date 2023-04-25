89bafac82db8d9c9d6867aba7a04d539b48d2204
avalon <asd95027@gmail.com>
2023-04-25 20:05:07 +0800

    完成ajax初步呼叫

---
+++ static/pm25-charts.js
@@ -0,0 +1,41 @@
+// 基于准备好的dom，初始化echarts实例
+const chart = echarts.init(document.querySelector('#main'));
+
+$.ajax(
+    {
+        url: "/pm25-data",
+        type: "GET",
+        dataType: "json",
+        success: (data) => {
+            console.log(data);
+        },
+        error: () => {
+            alert("取得資料失敗");
+        }
+    }
+);
+
+// 指定图表的配置项和数据
+let option = {
+    title: {
+        text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    legend: {
+        data: ['销量']
+    },
+    xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {},
+    series: [
+        {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }
+    ]
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
