56b7ab08003cf0b91f31a21c914c072efbfe83cb
avalon <asd95027@gmail.com>
2023-04-27 20:41:23 +0800

    完成六都繪製

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from datetime import datetime
-from pm25 import get_pm25
+from pm25 import get_pm25, get_pm25_db, get_six_pm25
 import json
 
 app = Flask(__name__)
@@ -25,20 +25,42 @@ def pm25_charts():
     return render_template('./pm25-charts.html')
 
 
+@app.route('/pm25-six-data')
+def get_six_pm25_data():
+    result = get_six_pm25()
+    datas = {
+        'county': list(result.keys()),
+        'pm25': list(result.values()),
+    }
+
+    return json.dumps(datas, ensure_ascii=False)
+
+
 @app.route('/pm25-data', methods=['POST'])
 def get_pm25_data():
-    columns, values = get_pm25()
-    # 縣市
-    county = [value[1] for value in values]
-    # 站點名稱
-    site = [value[0] for value in values]
-    # pm2.5數值
-    pm25 = [value[2] for value in values]
-    datas = {
-        'county': county,
-        'site': site,
-        'pm25': pm25
-    }
+    columns, values = get_pm25_db()
+
+    datas = {'error': '連線錯誤!'}
+    if values is not None:
+        # 縣市
+        county = [value[1] for value in values]
+        # 站點名稱
+        site = [value[0] for value in values]
+        # pm2.5數值
+        pm25 = [value[2] for value in values]
+
+        result = list(zip(site, pm25))
+        sorted_data = sorted(result, key=lambda x: x[-1])
+        print(sorted_data)
+
+        datas = {
+            'county': county,
+            'site': site,
+            'pm25': pm25,
+            'highest': sorted_data[-1],
+            'lowest': sorted_data[0],
+            'date': get_date()
+        }
 
     return json.dumps(datas, ensure_ascii=False)
 
@@ -46,15 +68,12 @@ def get_pm25_data():
 @app.route('/pm25', methods=['GET', 'POST'])
 def pm25():
     if request.method == 'GET':
-        columns, values = get_pm25()
-        # 使用GET => request.args.get(name)
-        # if request.args.get('sort'):
-        #     olumns, values = get_pm25(True)
+        columns, values = get_pm25_db()
     if request.method == 'POST':
         if request.form.get('sort'):
-            columns, values = get_pm25(True)
+            columns, values = get_pm25_db(True)
         else:
-            columns, values = get_pm25()
+            columns, values = get_pm25_db()
 
     print(columns, values)
 
